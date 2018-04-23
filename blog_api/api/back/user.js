0e79348d475f1e7a81033b3b3203fefeea50aa19
guoyushi <820529826@qq.com>
2018-04-23 09:31:21 +0800

    guoyushi

---
+++ blog_api/api/back/user.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var hasOwnProperty=require('has-own-property-x');  //支持hasOwnProperty
+var {
+  sqlHandle,  //除查询外的其他操作
+  readHandle,  //读取操作
+  searchHandle,  //检测有无某条数据，有为false
+  searchHandleNormal,  //检测有无某条数据，有为true
+  query             // //基本操作
+}=require("../../config/db_connect")
+
+
+/* GET home page. */
+
+router.post('/login', function(req, res, next) {
+  let arg=req.body
+  if(hasOwnProperty(arg,"username")&&hasOwnProperty(arg,"password")){
+    let {username,password}=arg
+    var testUser=`select * from user where name='${username}'`
+    readHandle(testUser).then((data)=>{
+        if(data.length>0){
+          if(data[0].password==password){
+                  res.send({
+                    code:"1001",
+                    msg:"登录成功",
+                    data:data[0]
+                  })
+          }else{
+            res.send({
+              code:"1002",
+              msg:"密码错误"
+            })
+          }
+        }else{
+          res.send({
+            code:"1003",
+            msg:"用户名不存在"
+          })
+        }
+    })
+  }else{
+    res.send({
+      code:"1000",
+      msg:"未输入用户名或密码"
+    })
+  }
+});
+module.exports = router;
