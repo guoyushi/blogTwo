e50a3b5138a690dd854d69b97e89b5c2bb767e60
gao18516823865 <18516823865@163.com>
2018-04-24 13:33:38 +0800

    aa

---
+++ blog _back/src/components/main/api_list.vue
@@ -0,0 +1,94 @@
+<template>
+        <div class="api_table">
+        <el-table
+            :data="q.slice((currentPage-1)*pageSize,currentPage*pageSize)"
+            border
+            style="width: 100%">
+            <el-table-column
+            label="标题"
+            width="180">
+            <template slot-scope="scope">
+                <span style="margin-left: 10px">{{ scope.row.cnname }}</span>
+            </template>
+            </el-table-column>
+            <el-table-column
+            label="前后端分类"
+            width="180">
+            <template slot-scope="scope">
+                <span style="margin-left: 10px">back</span>
+            </template>
+            </el-table-column>
+            <el-table-column
+            label="时间">
+                <template slot-scope="scope">
+                    <span style="margin-left: 10px">{{ scope.row.time }}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作">
+            <template slot-scope="scope">
+                <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+            </el-table-column>
+        </el-table>
+            <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="currentPage"
+            :page-sizes="[1,5, 10,15]"
+            :page-size="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="q.length">
+            </el-pagination>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                data:'',
+                q:[],
+                 currentPage:1,
+                pageSize:5
+            }
+        },
+        mounted(){
+            console.log(this.$route.query)
+            this.axios.get('/api/back/add/api_q').then(data=>{
+                this.data=data.data.data
+                const ts='前台接口'
+                this.data.forEach(i=>{
+                    if(i.type_one==ts){
+                        this.q.push(i)
+                    }
+                })
+            })
+        },
+        methods:{
+            handleEdit(index,row){
+                console.log(row)
+                this.$router.push({path:'api_add'})
+            },
+            handleDelete(index,row){
+
+            },
+            handleSizeChange(val) {
+                this.pageSize=val
+            console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                this.currentPage=val
+            }   
+        }
+    }
+</script>
+<style>
+    .right{
+        float:right;
+    }
+</style>
---
+++ blog _back/src/main.js
@@ -0,0 +1,47 @@
+
+// import aa from "./js/common.js"
+// console.log(aa)
+// import app from "./components/app.vue"
+// import Vue from " vue/dist/vue.js ";
+import Vue from "vue"
+import router from "./router"
+import store from "./store"
+import "./directives/index"
+import "./components/register.js"
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import vueAxios from "vue-axios"
+import axios from "axios"
+
+Vue.use(vueAxios,axios)
+//编辑图版
+import './static/Ueditor/ueditor.config.js'
+import './static/Ueditor/ueditor.all.min.js'
+import './static/Ueditor/lang/zh-cn/zh-cn.js'
+import './static/Ueditor/ueditor.parse.js'
+
+
+router.beforeEach((to,from,next)=>{
+  if(to.path=="/login"){
+    next()
+  }else{
+    if(sessionStorage.getItem("userId")){
+      next()
+    }else{
+      next("/login")
+    }
+  }
+})
+
+new Vue({
+  el:"#root",
+  router,
+  store,
+  template:"<app />",
+  components: {
+      app: resolve => require(['./components/app.vue'], resolve)
+  }
+})
